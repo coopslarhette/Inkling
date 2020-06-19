e0f4a9778a1fb3bd6c6e5c52dac5b0ad1b2a2b24
coopslarhette <coopslarhette@gmail.com>
2020-06-19 15:05:23 -0700

    moves builtin generators to generator-helpers.js

--- ast/nodes/call.js
+++ ast/nodes/call.js
@@ -1,48 +1,6 @@
 const check = require('../../semantics/check')
 const FuncDecStmt = require('./func-dec-statement')
-
-const builtin = {
-  xProcess(code) {
-    return `process.exit(${code})`
-  },
-  slice([s, begin, end]) {
-    return `${s}.slice(${begin}, ${end})`
-  },
-  length([s]) {
-    return `${s}.length`
-  },
-  charAt([s, i]) {
-    return `${s}.charAt(${i})`
-  },
-  abs([x]) {
-    const num = `${x}`.replace(/[()]/g, '')
-    return `Math.abs(${num})`
-  },
-  sqrt([x]) {
-    return `Math.sqrt(${x})`
-  },
-  random([start, end]) {
-    return `Math.floor(Math.random() * ${end} + ${start})`
-  },
-  pow([base, power]) {
-    return `${base}**${power}`
-  },
-  add([listId, value]) {
-    return `${listId.replace(/[''""]/g, '')}.push(${value})`
-  },
-  insert([listId, index, value]) {
-    return `${listId.replace(/[''""]/g, '')}.splice(${index}, 0, ${value})`
-  },
-  prepend([listId, value]) {
-    return `${listId.replace(/[''""]/g, '')}.prepend(${value})`
-  },
-  remove([listId]) {
-    return `${listId.replace(/[''""]/g, '')}.pop()`
-  },
-  range([start, end]) {
-    return `Array(${end} - ${start} + 1).fill().map((_, idx) => ${start} + idx)`
-  },
-}
+const genHelp = require('../../backend/generator-helpers')
 
 module.exports = class Call {
   constructor(id, args) {
@@ -71,7 +29,7 @@ module.exports = class Call {
   gen() {
     const args = this.args.map((a) => a.gen())
     if (this.callee.builtin) {
-      return builtin[this.callee.id](args)
+      return genHelp.builtinGenerators[this.callee.id](args)
     }
     return `${this.id.gen()}(${args.join()})`
   }
--- backend/generator-helpers.js
+++ backend/generator-helpers.js
@@ -16,8 +16,53 @@ const javaScriptId = (() => {
   }
 })()
 
+const builtinGenerators = {
+  xProcess(code) {
+    return `process.exit(${code})`
+  },
+  slice([s, begin, end]) {
+    return `${s}.slice(${begin}, ${end})`
+  },
+  length([s]) {
+    return `${s}.length`
+  },
+  charAt([s, i]) {
+    return `${s}.charAt(${i})`
+  },
+  abs([x]) {
+    const num = `${x}`.replace(/[()]/g, '')
+    return `Math.abs(${num})`
+  },
+  sqrt([x]) {
+    return `Math.sqrt(${x})`
+  },
+  random([start, end]) {
+    return `Math.floor(Math.random() * ${end} + ${start})`
+  },
+  pow([base, power]) {
+    return `${base}**${power}`
+  },
+  add([listId, value]) {
+    return `${listId.replace(/[''""]/g, '')}.push(${value})`
+  },
+  insert([listId, index, value]) {
+    return `${listId.replace(/[''""]/g, '')}.splice(${index}, 0, ${value})`
+  },
+  prepend([listId, value]) {
+    const cleanedListId = listId.replace(/[''""]/g, '')
+    return `${javaScriptId(cleanedListId)}.prepend(${value})`
+  },
+  remove([listId]) {
+    return `${listId.replace(/[''""]/g, '')}.pop()`
+  },
+  range([start, end]) {
+    return `Array(${end} - ${start} + 1).fill().map((_, idx) => ${start} + idx)`
+  },
+}
+
 
 module.exports = {
   makeOp,
   javaScriptId,
+  builtinGenerators,
 }
